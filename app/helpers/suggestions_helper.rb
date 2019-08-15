d7cf04afa452ab9c7617182d04e8d3b7a8e945f6
Vitor Tiba <vitor.hitiba@gmail.com>
2019-08-15 20:24:59 -0300

    Adds a helper to manipulate suggestion's attributes and uses env to configure database

--- app/helpers/suggestions_helper.rb
+++ app/helpers/suggestions_helper.rb
@@ -1,2 +1,11 @@
 module SuggestionsHelper
+	PERMITTED_DAYS = [13, 14, 15]
+
+	def can_show_all_suggestions?(date)
+		 PERMITTED_DAYS.include?(date.day.to_i)
+	end
+
+	def format_date_ptbr(date)
+		date.strftime("%d/%m/%y")
+	end
 end
